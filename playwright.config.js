290a0573e865745e396b2c283f2787dd66fc4dd7
Yjaswanthreddy <102424257+Yjaswanthreddy@users.noreply.github.com>
2026-03-23 07:22:50 +0530

    first commit

--- playwright.config.js
+++ playwright.config.js
@@ -102,8 +102,9 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
 
-  reporter: [['html'], 
-['allure-playwright']
+  reporter: [
+    ['html'], 
+    ['allure-playwright']
 ],
 
   use: {
